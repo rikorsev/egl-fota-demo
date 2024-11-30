243bef0d0de6fb9179f97799c5905650019237af
Alexandr Kolosov <rikorsev@gmail.com>
2024-11-30 23:15:07 +0200

    rfm69: Implement data modulation test

--- apps/main/rfm_test.c
+++ apps/main/rfm_test.c
@@ -48,7 +48,7 @@ static egl_result_t rfm_bitrate_test_run(void)
 static egl_result_t rfm_mode_test_run(void)
 {
     egl_result_t result;
-    egl_rfm_mode_t mode;
+    egl_rfm69_mode_t mode;
 
     result = egl_rfm69_mode_get(PLAT_RFM69, &mode);
     if(result != EGL_SUCCESS)
@@ -63,6 +63,62 @@ static egl_result_t rfm_mode_test_run(void)
     if(result != EGL_SUCCESS)
     {
         EGL_LOG_ERROR("Fail to set rfm69 mode. Result: %s", EGL_RESULT(result));
+        return result;
+    }
+
+    return result;
+}
+
+static egl_result_t rfm_data_modul_test_run(void)
+{
+    egl_result_t result;
+    egl_rfm69_modulation_shaping_t modshap;
+    egl_rfm69_modulation_type_t    modtype;
+    egl_rfm69_data_mode_t          datamode;
+
+    result = egl_rfm69_modulation_shaping_get(PLAT_RFM69, &modshap);
+    if(result != EGL_SUCCESS)
+    {
+        EGL_LOG_ERROR("Fail to get modulation shaping. Result: %s", EGL_RESULT(result));
+        return result;
+    }
+
+    result = egl_rfm69_modulation_type_get(PLAT_RFM69, &modtype);
+    if(result != EGL_SUCCESS)
+    {
+        EGL_LOG_ERROR("Fail to get modulation type. Result: %s", EGL_RESULT(result));
+        return result;
+    }
+
+    result = egl_rfm69_data_mode_get(PLAT_RFM69, &datamode);
+    if(result != EGL_SUCCESS)
+    {
+        EGL_LOG_ERROR("Fail to get data mode. Result: %s", EGL_RESULT(result));
+        return result;
+    }
+
+    EGL_LOG_INFO("Modulation shaping: %u", modshap);
+    EGL_LOG_INFO("Modulation type: %u", modtype);
+    EGL_LOG_INFO("Data mode: %u", datamode);
+
+    result = egl_rfm69_modulation_shaping_set(PLAT_RFM69, EGL_RFM69_MODULATION_SHAPING_1);
+    if(result != EGL_SUCCESS)
+    {
+        EGL_LOG_ERROR("Fail to set modulation shaping. Result: %s", EGL_RESULT(result));
+        return result;
+    }
+
+    result = egl_rfm69_modulation_type_set(PLAT_RFM69, EGL_RFM69_MODULATION_TYPE_OOK);
+    if(result != EGL_SUCCESS)
+    {
+        EGL_LOG_ERROR("Fail to set modulation type. Result: %s", EGL_RESULT(result));
+        return result;
+    }
+
+    result = egl_rfm69_data_mode_set(PLAT_RFM69, EGL_RFM69_DATA_MODE_CONTINIOUS_W_SYNC);
+    if(result != EGL_SUCCESS)
+    {
+        EGL_LOG_ERROR("Fail to set data mode. Result: %d", EGL_RESULT(result));
         return result;
     }
 
@@ -89,6 +145,12 @@ void rfm_test_run(void)
     if(result != EGL_SUCCESS)
     {
         EGL_LOG_ERROR("Mode test fail. Result: %s", EGL_RESULT(result));
+    }
+
+    result = rfm_data_modul_test_run();
+    if(result != EGL_SUCCESS)
+    {
+        EGL_LOG_ERROR("Data modul test fail. Result: %s", EGL_RESULT(result));
     }
 }
 
