f282b9ed79c34eda2e6d5eacd6ef63a19ccd75ad
Alexandr Kolosov <rikorsev@gmail.com>
2026-02-02 23:59:13 +0200

    syslog: Implement syslog over thread

--- plat/stm32l073-nucleo/syslog/plat_syslog.c
+++ plat/stm32l073-nucleo/syslog/plat_syslog.c
@@ -2,9 +2,17 @@
 #include "plat.h"
 
 #define BAUDRATE_115200 (0x12U)
+#define SYSLOG_THREAD_PRIORITY (16U)
+#define SYSLOG_MSG_CHUNK_SIZE (32U)
 
 extern egl_timer_t plat_systimer_inst;
 static char syslog_buff[CONFIG_PLAT_SYSLOG_BUFF_SIZE];
+
+#if CONFIG_EGL_OS_ENABLED
+static void *syslog_thread = NULL;
+static void *syslog_queue = NULL;
+static void *syslog_mux = NULL;
+#endif
 
 static void init_tx_pin(void)
 {
@@ -53,7 +61,7 @@ static egl_result_t init(void)
     return EGL_SUCCESS;
 }
 
-static egl_result_t write(void *data, size_t *len)
+static egl_result_t write_uart(void *data, size_t *len)
 {
     for(uint32_t i = 0; i < *len; i++)
     {
@@ -65,6 +73,104 @@ static egl_result_t write(void *data, size_t *len)
     }
 
     return EGL_SUCCESS;
+}
+
+#if CONFIG_EGL_OS_ENABLED
+static egl_result_t write_queue(void *data, size_t *len)
+{
+    egl_result_t result;
+
+    result = egl_os_mux_lock(SYSOS, syslog_mux, EGL_OS_WAIT_FOREWER);
+    EGL_RESULT_CHECK(result);
+
+    for(unsigned int i = 0; i < *len; i += SYSLOG_MSG_CHUNK_SIZE)
+    {
+        uint8_t chunk[SYSLOG_MSG_CHUNK_SIZE] = {0};
+        size_t size = i + SYSLOG_MSG_CHUNK_SIZE > *len ? *len - i : SYSLOG_MSG_CHUNK_SIZE;
+
+        memcpy(chunk, &((uint8_t *)data)[i], size);
+
+        result = egl_os_queue_put(SYSOS, syslog_queue, chunk, EGL_OS_WAIT_FOREWER);
+        EGL_RESULT_CHECK_EXIT(result);
+    }
+    
+exit:
+    result = egl_os_mux_unlock(SYSOS, syslog_mux);
+    EGL_RESULT_CHECK(result);
+
+    return result;
+}
+
+static void plat_syslog_thread_entry(void *data)
+{
+    while(1)
+    {
+        uint8_t chunk[SYSLOG_MSG_CHUNK_SIZE] = {0};
+        size_t size = sizeof(chunk);
+        egl_os_queue_get(SYSOS, syslog_queue, &chunk, EGL_OS_WAIT_FOREWER);
+        write_uart(chunk, &size);
+    }
+}
+
+static egl_result_t init_thread(void)
+{
+    egl_result_t result;
+    static egl_os_thread_ctx thread_ctx;
+    static egl_os_queue_ctx queue_ctx;
+    static egl_os_mux_ctx mux_ctx;
+    static uint8_t stack[1024];
+    static uint8_t queue[CONFIG_PLAT_SYSLOG_BUFF_SIZE];
+
+    result = egl_os_mux_create(SYSOS, &syslog_mux, "syslog_mux",
+                               EGL_OS_MUX_TYPE_REGULAR, &mux_ctx);
+    EGL_RESULT_CHECK(result);
+
+    result = egl_os_queue_create(SYSOS, &syslog_queue, "syslog_queue",
+                                 CONFIG_PLAT_SYSLOG_BUFF_SIZE, SYSLOG_MSG_CHUNK_SIZE,
+                                 queue, sizeof(queue), &queue_ctx);
+    EGL_RESULT_CHECK(result);
+
+    result = egl_os_thread_create(SYSOS, &syslog_thread, "syslog",
+                                  plat_syslog_thread_entry,
+                                  NULL, stack, sizeof(stack),
+                                  SYSLOG_THREAD_PRIORITY, &thread_ctx);
+    EGL_RESULT_CHECK(result);
+
+    return result;
+}
+#endif
+
+static egl_result_t write(void *data, size_t *len)
+{
+    egl_result_t result;
+#if CONFIG_EGL_OS_ENABLED
+    if(__builtin_expect(syslog_thread == NULL, 0))
+    {
+        egl_os_state_t state;
+        result = egl_os_state_get(SYSOS, &state);
+        EGL_RESULT_CHECK(result);
+
+        if(state == EGL_OS_RUNNING)
+        {
+            result = init_thread();
+            EGL_RESULT_CHECK(result);
+        }
+        else
+        {
+#endif
+            result = write_uart(data, len);
+            EGL_RESULT_CHECK(result);
+#if CONFIG_EGL_OS_ENABLED
+        }
+    }
+    else
+    {
+        result = write_queue(data, len);
+        EGL_RESULT_CHECK(result);
+    }
+#endif
+
+    return result;
 }
 
 static const egl_iface_t plat_syslog_iface =
